110e4c48838cb209bba98de10f43bb62d2b72cf3
Deary <chickenflakes9@gmail.com>
2019-03-27 08:33:17 +0700

    First initialize with error

---
+++ client/main.js
@@ -0,0 +1,10 @@
+import { Template } from 'meteor/templating';
+import { Notes } from '../lib/collections.js';
+
+import './main.html';
+
+Template.body.helpers({
+  notes() {
+    return Notes.find({});
+  }
+});
